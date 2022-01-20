ae7887396d97dfec0cfee8c25c76289b12916403
abdulkarim <abdulkarim@synergix.com>
2022-01-20 22:19:09 +0200

    Update Licenser to .NetFramework5.2

--- EEMS/My Project/Settings.Designer.vb
+++ EEMS/My Project/Settings.Designer.vb
@@ -15,7 +15,7 @@ Option Explicit On
 Namespace My
     
     <Global.System.Runtime.CompilerServices.CompilerGeneratedAttribute(),  _
-     Global.System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.8.1.0"),  _
+     Global.System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.1.0.0"),  _
      Global.System.ComponentModel.EditorBrowsableAttribute(Global.System.ComponentModel.EditorBrowsableState.Advanced)>  _
     Partial Friend NotInheritable Class MySettings
         Inherits Global.System.Configuration.ApplicationSettingsBase
@@ -54,17 +54,6 @@ Namespace My
             End Get
         End Property
         
-        <Global.System.Configuration.ApplicationScopedSettingAttribute(),  _
-         Global.System.Diagnostics.DebuggerNonUserCodeAttribute(),  _
-         Global.System.Configuration.SpecialSettingAttribute(Global.System.Configuration.SpecialSetting.ConnectionString),  _
-         Global.System.Configuration.DefaultSettingValueAttribute("Server=localhost;Database=EEMS-Majdal;Persist Security Info=True;Integrated Secur"& _ 
-            "ity=true;user id=sa;password=mohammadfneish;")>  _
-        Public ReadOnly Property EEMSConnectionString() As String
-            Get
-                Return CType(Me("EEMSConnectionString"),String)
-            End Get
-        End Property
-        
         <Global.System.Configuration.UserScopedSettingAttribute(),  _
          Global.System.Diagnostics.DebuggerNonUserCodeAttribute(),  _
          Global.System.Configuration.DefaultSettingValueAttribute("admin")>  _
@@ -75,6 +64,17 @@ Namespace My
             Set
                 Me("lastuser") = value
             End Set
+        End Property
+        
+        <Global.System.Configuration.ApplicationScopedSettingAttribute(),  _
+         Global.System.Diagnostics.DebuggerNonUserCodeAttribute(),  _
+         Global.System.Configuration.SpecialSettingAttribute(Global.System.Configuration.SpecialSetting.ConnectionString),  _
+         Global.System.Configuration.DefaultSettingValueAttribute("Server=localhost;Database=EEMS-Majdal;Persist Security Info=True;Integrated Secur"& _ 
+            "ity=true;user id=sa;password=mohammadfneish;")>  _
+        Public ReadOnly Property EEMSConnectionString() As String
+            Get
+                Return CType(Me("EEMSConnectionString"),String)
+            End Get
         End Property
     End Class
 End Namespace
