abb1172187c9bd3b99e157dd679ba1d446116d37
abdulkarim <abdulkarim@DC011>
2022-01-28 16:13:00 -0800

    change build version to 2.5.0.0

--- EEMS/My Project/AssemblyInfo.vb
+++ EEMS/My Project/AssemblyInfo.vb
@@ -20,7 +20,7 @@ Imports System.Runtime.InteropServices
 <Assembly: ComVisible(False)> 
 
 'The following GUID is for the ID of the typelib if this project is exposed to COM
-<Assembly: Guid("5ae5ec5c-cf6a-42ac-8014-845e9b76d84b")> 
+<Assembly: Guid("5ae5ec5c-cf6a-42ac-8014-845e9b76d84b")>
 
 ' Version information for an assembly consists of the following four values:
 '
@@ -33,7 +33,7 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("2.4.4.1")> 
-<Assembly: AssemblyFileVersion("2.4.4.1")> 
+<Assembly: AssemblyVersion("2.5.0.0")>
+<Assembly: AssemblyFileVersion("2.5.0.0")>
 
 <Assembly: NeutralResourcesLanguageAttribute("ar")> 
